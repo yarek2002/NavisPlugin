205a9e50f70db978d34cb34cd42e34a632a8e0bc
yarek2002 <yarsen2002@gmail.com>
2025-10-28 12:36:40 +0300

    feat: save columns propeties

--- mainPlugin-master/ClashManager/ManagerCollision/Views/ManagerCollisionView.xaml.cs
+++ mainPlugin-master/ClashManager/ManagerCollision/Views/ManagerCollisionView.xaml.cs
@@ -2971,7 +2971,7 @@ namespace ClashManager.ManagerCollision.Views
 		}
 
 		/// <summary>
-		/// Load saved GridView column order and widths (if file exists)
+		/// Load saved GridView column order and widths
 		/// Matching by Tag first, then by Header string. Unknown columns left in their current order.
 		/// </summary>
 		ptivate void LoadColumnLayout()
